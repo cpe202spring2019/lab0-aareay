ae156c92fb6185ac4744319e51b25b65ecf82ae3
aareay <aareay@gmail.com>
2019-04-05 12:14:00 -0700

    Added prompt for earth weight, output converting earth weight to Mars and Jupiter weight

--- planets.py
+++ planets.py
@@ -1,6 +1,10 @@
 def weight_on_planets():
    # write your code here
-   
+    earth_weight = int(input("What do you weigh on earth? "))
+    mars_weight = earth_weight * 0.38
+    jupiter_weight = earth_weight * 2.34
+    print("\nOn Mars you would weigh {0} pounds.\nOn Jupiter you would weigh"
+        " {1} pounds.".format(mars_weight, jupiter_weight))
    
    
 if __name__ == '__main__':
